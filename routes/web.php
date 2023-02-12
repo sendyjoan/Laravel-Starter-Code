93b4aa115c4f26fdcd342fd6d00869f4a6d5f502
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2023-02-12 03:32:10 -0500

    Add Auth Layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.dashboard');
 });
+
+Route::get('login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('forget', function () {
+    return view('auth.forget');
+})->name('forget');
