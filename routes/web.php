d561921917dc98a2dc9466ea9075bff4e1af682e
abhisharma278 <shaarmaabhi27@gmail.com>
2023-10-26 21:06:57 +0530

    Make EndPints in Application

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/greetings', function () {
+    return 'Hello World';
+}); 
+Route::get('/user', [UserController::class, 'index']);
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
